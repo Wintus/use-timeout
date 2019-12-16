b495921c24e9561fe565efc35122082f1e91bf07
wintus <yuya0726+GitHub@gmail.com>
2019-12-16 17:12:49 +0900

    Rename param for doc

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from "react";
 export const useTimeout = <S>(
   initial: S,
   fallback: S,
-  timeout: number
+  timeoutMs: number
 ): [S, React.Dispatch<React.SetStateAction<S>>] => {
   const [state, setState] = useState<S>(initial);
 
   useEffect(() => {
-    const timer = setTimeout(() => setState(fallback), timeout);
+    const timer = setTimeout(() => setState(fallback), timeoutMs);
     return () => clearTimeout(timer);
-  }, [state, fallback, timeout]);
+  }, [state, fallback, timeoutMs]);
 
   return [state, setState];
 };
